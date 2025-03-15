30fc938d8a1e46cafac2c33b8d690e23e5f5b9b1
Nevena Ilieva <118193964+nevenailievaa@users.noreply.github.com>
2025-03-15 13:17:32 +0200

    Update Program.cs

--- 03. Conditional Statements Advanced/01. Lab/05.SmallShop/Program.cs
+++ 03. Conditional Statements Advanced/01. Lab/05.SmallShop/Program.cs
@@ -1,78 +1,72 @@
-﻿//Input
+//Input
 string product = Console.ReadLine();
 string town = Console.ReadLine();
 double quantity = double.Parse(Console.ReadLine());
 
+//Calculations
 double price = 0;
 
-//Calculations
 if (town == "Sofia")
 {
-    if (product == "coffee")
+    switch (product)
     {
-        price = 0.50;
-    }
-    else if (product == "water")
-    {
-        price = 0.80;
-    }
-    else if (product == "beer")
-    {
-        price = 1.20;
-    }
-    else if (product == "sweets")
-    {
-        price = 1.45;
-    }
-    else if (product == "peanuts")
-    {
-        price = 1.60;
+        case "coffee":
+            price = 0.50;
+            break;
+        case "water":
+            price = 0.80;
+            break;
+        case "beer":
+            price = 1.20;
+            break;
+        case "sweets":
+            price = 1.45;
+            break;
+        case "peanuts":
+            price = 1.60;
+            break;
     }
 }
 else if (town == "Plovdiv")
 {
-    if (product == "coffee")
+    switch (product)
     {
-        price = 0.40;
-    }
-    else if (product == "water")
-    {
-        price = 0.70;
-    }
-    else if (product == "beer")
-    {
-        price = 1.15;
-    }
-    else if (product == "sweets")
-    {
-        price = 1.30;
-    }
-    else if (product == "peanuts")
-    {
-        price = 1.50;
+        case "coffee":
+            price = 0.40;
+            break;
+        case "water":
+            price = 0.70;
+            break;
+        case "beer":
+            price = 1.15;
+            break;
+        case "sweets":
+            price = 1.30;
+            break;
+        case "peanuts":
+            price = 1.50;
+            break;
     }
 }
 else if (town == "Varna")
 {
-    if (product == "coffee")
+    switch (product)
     {
-        price = 0.45;
-    }
-    else if (product == "water")
-    {
-        price = 0.70;
-    }
-    else if (product == "beer")
-    {
-        price = 1.10;
-    }
-    else if (product == "sweets")
-    {
-        price = 1.35;
-    }
-    else if (product == "peanuts")
-    {
-        price = 1.55;
+        case "coffee":
+            price = 0.45;
+            break;
+        case "water":
+            price = 0.70;
+            break;
+        case "beer":
+            price = 1.10;
+            break;
+        case "sweets":
+            price = 1.35;
+            break;
+        case "peanuts":
+            price = 1.55;
+            break;
     }
 }
 
