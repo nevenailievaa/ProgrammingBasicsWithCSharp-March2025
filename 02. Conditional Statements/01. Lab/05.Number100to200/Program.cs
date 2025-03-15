e96482840cbec3e14e8bbd16e86d4172834845be
Nevena Ilieva <118193964+nevenailievaa@users.noreply.github.com>
2025-03-15 13:14:40 +0200

    Update Program.cs

--- 02. Conditional Statements/01. Lab/05.Number100to200/Program.cs
+++ 02. Conditional Statements/01. Lab/05.Number100to200/Program.cs
@@ -1,16 +1,16 @@
-﻿//Input
+//Input
 int number = int.Parse(Console.ReadLine());
 
 //Checks
-if (number < 100)
+if (number > 200)
 {
-    Console.WriteLine("Less than 100");
+    Console.WriteLine("Greater than 200");
 }
-else if (number >= 100 && number <= 200)
+else if (number >= 100)
 {
     Console.WriteLine("Between 100 and 200");
 }
-else if (number > 200)
+else
 {
-    Console.WriteLine("Greater than 200");
+    Console.WriteLine("Less than 100");
 }
