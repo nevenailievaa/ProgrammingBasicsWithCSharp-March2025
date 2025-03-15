e5484a4f3969cee2e47046753b010d253d318469
kalata23 <kaloyanvn@gmail.com>
2025-03-15 13:32:08 +0200

    Update Program.cs
    Reworked code according to the lectures.

--- 02. Conditional Statements/01. Lab/06.SpeedInfo/Program.cs
+++ 02. Conditional Statements/01. Lab/06.SpeedInfo/Program.cs
@@ -6,19 +6,19 @@ if (speed <= 10)
 {
     Console.WriteLine("slow");
 }
-else if (speed > 10 && speed <= 50)
+else if (speed <= 50)
 {
     Console.WriteLine("average");
 }
-else if (speed > 50 && speed <= 150)
+else if (speed <= 150)
 {
     Console.WriteLine("fast");
 }
-else if (speed > 150 && speed <= 1000)
+else if (speed <= 1000)
 {
     Console.WriteLine("ultra fast");
 }
-else if (speed > 1000)
+else
 {
     Console.WriteLine("extremely fast");
 }
